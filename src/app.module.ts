a2bb17b293204ac66974dbda03932f2fd612f2ab
Pettecco <petterson.ikaro@gec.inatel.br>
2025-09-10 21:45:02 -0300

    chore: setup TypeORM, ConfigService, .env.example and DataSource
    Added TypeORM and configured PostgreSQL connection
    Integrated ConfigModule and ConfigService for environment variables
    Created .env.example with all necessary variables
    Added data-source.ts for TypeORM migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: +config.get('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+      }),
+    }),
+  ],
+  providers: [],
 })
 export class AppModule {}
